d9444d810475e13c201139db71671f2742b90ad1
Aldane Purrier <aldanepur@gmail.com>
2019-06-24 15:19:27 -0400

    it works!

---
+++ server.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "faker"
+
+$db = {"rock" => "lee", "chris" => "rock", "scooby" => "doo"}
+
+get "/" do
+  @person = Faker::Name.name
+  erb :home
+end
+
+get "/about" do
+  erb :about
+end
+
+get '/skills' do
+  erb :skills
+end
+
+get '/results' do
+  def getResults
+    @result = $db[params[:search]]
+    if @result == nil
+      @result = "Please provide valid search"
+    end
+  end
+getResults()
+erb :results
+end
